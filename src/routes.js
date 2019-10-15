364e083b6cb00d2c21c5b95795d555db5347dd0d
Dan Garcia <daniel.of@gmail.com>
2019-10-15 20:39:34 -0300

    Creating User Store Route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 // const { Router } = require('express');  sem o sucrase instalado
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Omni' });
-});
+routes.post('/users', UserController.store);
 
 // module.exports = routes;
 export default routes;
