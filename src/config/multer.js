96f6285b4b277626678a8f76d4f98ecd39162d32
Dan Garcia <daniel.of@gmail.com>
2019-10-17 11:32:10 -0300

    file upload: multer configuration

---
+++ src/config/multer.js
@@ -0,0 +1,19 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        /* caso não haja erros retornar o nome do arquivo como:
+         os 16bytes gerados aleatoriamente convertidos para str hexadec +
+         extensão do arquivo */
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 // const { Router } = require('express');  sem o sucrase instalado
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,6 +16,11 @@ routes.post('/sessions', SessionController.store);
 // middleware global para todas as rotas subsequentes
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+// a requisição será enviada no campo file
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 // module.exports = routes;
 export default routes;
