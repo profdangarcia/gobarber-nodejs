7bc0bbc4320c7c2676bf33ab72e3dd799027b994
Dan Garcia <daniel.of@gmail.com>
2019-10-19 12:08:15 -0300

    Configuring E-mail: Nodemailer + Mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd35cce536838e8',
+    pass: '8a801291bbd735',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
