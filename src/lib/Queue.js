54e619def3a2d02d15500644a1e356cd6d7457f3
Dan Garcia <daniel.of@gmail.com>
2019-10-20 12:50:13 -0300

    Using Queues

---
+++ src/lib/Queue.js
@@ -0,0 +1,41 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  // método para criar uma conexão do job com o redis
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  // método para adicionar um novo job à fila
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  // método para executar um job da fila
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
