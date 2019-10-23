1c5ec0e154bc734225b667bdb1dd774a225b4954
Dan Garcia <daniel.of@gmail.com>
2019-10-22 21:05:05 -0300

    Handling Exceptions

--- src/app.js
+++ src/app.js
@@ -4,19 +4,31 @@
 
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+// express-async-errors precisa vir antes das rotas
+import 'express-async-errors';
 import routes from './routes';
-// não é necessário o index.js
+
 import './database';
 
 class App {
   // constructor sempre será chamado ao se instanciar a classe
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+    // foi colocado dentro dos middlewares
+    // this.server.use(Sentry.Handlers.requestHandler());
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -26,6 +38,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  // a url vem do site do sentry, fornecida no projeto
+  dsn: 'https://1fca41e11df24164a896d11d06335c90@sentry.io/1793017',
+};
